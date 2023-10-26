a03ba96320729d4958ff37d05ffdeafe238a9fb3
Son Pham <sopham@strongtie.com>
2023-10-26 04:13:22 +0000

    "First commit"

---
+++ src/test/java/vn/unigap/RecruitmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.unigap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecruitmentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
